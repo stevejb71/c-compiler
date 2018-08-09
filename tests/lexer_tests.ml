0db7f84a14b271c81d918c2089a0c602a8ede37f
steve <stevejb71@gmail.com>
2018-08-09 18:25:54 +0800

    Add file based stage 1 tests for lexer

--- tests/lexer_tests.ml
+++ tests/lexer_tests.ml
@@ -1,6 +1,6 @@
 open OUnit2
 open Lexer
-open Base
+open Core_kernel
 
 let print_token = function
 | OPEN_CURLY -> "<OPEN_CURLY>"
@@ -17,6 +17,17 @@ let assert_ok exp (got: (token list, string) Result.t) _ctxt =
 let assert_error (exp: string) (got: ('a, string) Result.t) _ctxt = 
   let got = Option.value_exn (Result.error got) in
   assert_equal exp got ~printer:Fn.id
+
+let assert_can_lex_file (filename: string) _ctxt =
+  let program = In_channel.read_all filename in
+  match lex program with
+  | Ok _ -> ()
+  | Error msg -> failwith ("Failed: " ^ msg)
+  
+let assert_can_lex_files_in_folder (foldername: string) _ctxt =
+  let foldername = "../../../tests/" ^ foldername in
+  let c_files = Sys.readdir foldername in
+  Array.iter c_files ~f:(fun name -> assert_can_lex_file (foldername ^ "/" ^ name) _ctxt)
   
 let lexer_tests = [
   "empty string has no tokens" >::
@@ -31,4 +42,8 @@ let lexer_tests = [
     assert_ok [INT_LITERAL; IDENTIFIER; SEMICOLON] (lex "  7 somefunc  ; ");
   "reports an error on failing to lex " >::
     assert_error "Nothing matches at position 7" (lex "   doub:le");
+  "lexes valid C files" >::
+    assert_can_lex_files_in_folder "stage_1/valid";
+  "lexes invalid C files" >::
+    assert_can_lex_files_in_folder "stage_1/invalid";
 ]
