b92e5c9dcdfaf1eb149f570f6552771a98441cd7
steve <stevejb71@gmail.com>
2018-08-12 17:29:16 +0800

    Rejig compiler exe into smaller functions.

--- app/compiler.ml
+++ app/compiler.ml
@@ -3,12 +3,15 @@ open Asm
 open Lexer
 open Parser
 open Codegen
+open Functions
 
-let () =
+let compile_code code =
   let open Result.Monad_infix in
-  let filename = Sys.argv.(1) in
-  let code = In_channel.read_all filename in
   let emitter = emit_asm print_endline in
   match lex code >>= parse >>= (codegen emitter) with
   | Ok _ -> ()
   | Error msg -> print_endline msg
+
+let compile_file = compile_code >|> In_channel.read_all
+
+let () = compile_file Sys.argv.(1)
