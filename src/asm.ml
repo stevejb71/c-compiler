ca41d2e68b9833935c1b3368dcfde0c700859c33
steve <stevejb71@gmail.com>
2018-08-13 21:16:28 +0800

    Add assembly for stage 3

--- src/asm.ml
+++ src/asm.ml
@@ -2,6 +2,9 @@ open Base
 
 type register =
 | Eax
+| Rax
+| Ecx
+| Rcx
 | Al
 
 type t =
@@ -13,14 +16,26 @@ type t =
 | Not of register
 | Cmpl of {x:int; r:register}
 | Sete of register
+| Push of register
+| Pop of register
+| Addl of (register * register)
+| Subl of (register * register)
+| IMul of  (register * register)
+| IDivl of  (register * register)
 
 let reg_to_str = function
 | Eax -> "%eax"
+| Rax -> "%rax"
+| Ecx -> "%ecx"
+| Rcx -> "%rcx"
 | Al -> "%al"
 
 let op_reg_to_string operand r =
   Printf.sprintf "%s %s" operand (reg_to_str r)
 
+let binary_op_to_string operand (l, r) =
+  Printf.sprintf "%s %s,%s" operand (reg_to_str l) (reg_to_str r)
+  
 let asm_to_string = function
 | Globl s -> ".globl " ^ s
 | Label s -> s ^ ":"
@@ -30,5 +45,11 @@ let asm_to_string = function
 | Not r -> op_reg_to_string "not" r
 | Cmpl {x; r} -> Printf.sprintf "cmpl $%d, %s" x (reg_to_str r)
 | Sete r -> op_reg_to_string "sete" r
+| Addl rs -> binary_op_to_string "addl" rs
+| Subl rs -> binary_op_to_string "subl" rs
+| IMul rs -> binary_op_to_string "imul" rs
+| IDivl rs -> binary_op_to_string "idivl" rs
+| Push r -> op_reg_to_string "push" r
+| Pop r -> op_reg_to_string "pop" r
 
 let emit_asm (emitter: string -> unit) asm = emitter (asm_to_string asm)
