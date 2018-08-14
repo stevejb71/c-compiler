36e4b8eba1e5d3ddf04bcdc605ae0de677f77338
steve <stevejb71@gmail.com>
2018-08-14 21:32:08 +0800

    Code gen for stage 4

--- src/codegen.ml
+++ src/codegen.ml
@@ -9,8 +9,17 @@ let rec codegen_exp emitter asm =
     codegen_exp emitter e1;
     emitter (Push Rax);
     codegen_exp emitter e2;
-    emitter (Pop Rcx)
-  in match asm with
+    emitter (Pop Rcx) 
+  in
+  let emit_comparison e1 e2 =
+    codegen_exp emitter e1;
+    emitter (Push Rax);
+    codegen_exp emitter e2;
+    emitter (Pop Rcx);
+    emitter (Cmpl (R Eax, Ecx));
+    emitter (Movl (0, Eax));
+  in 
+  match asm with
   | Const x -> 
       emitter (Movl (x, Eax))
   | Complement e -> 
@@ -21,7 +30,7 @@ let rec codegen_exp emitter asm =
       emitter (Neg Eax)
   | Logical_Negation e -> 
       codegen_exp emitter e;
-      emitter (Cmpl (0, Eax));
+      emitter (Cmpl (I 0, Eax));
       emitter (Movl (0, Eax));
       emitter (Sete Al)
   | Addition (e1, e2) -> 
@@ -40,6 +49,44 @@ let rec codegen_exp emitter asm =
       codegen_exp emitter e1;
       emitter (Pop Rcx);
       emitter (IDivl (Ecx, Eax))
+  | Equality (e1, e2) ->
+      emit_comparison e1 e2;
+      emitter (Sete Al);
+  | Inequality (e1, e2) ->
+      emit_comparison e1 e2;
+      emitter (Setne Al);
+  | LessThan (e1, e2) ->
+      emit_comparison e1 e2;
+      emitter (Setl Al);
+  | LessThanOrEqual (e1, e2) ->
+      emit_comparison e1 e2;
+      emitter (Setle Al);
+  | GreaterThan (e1, e2) ->
+      emit_comparison e1 e2;
+      emitter (Setg Al);
+  | GreaterThanOrEqual (e1, e2) ->
+      emit_comparison e1 e2;
+      emitter (Setge Al)
+  | Logical_And (e1, e2) ->
+      codegen_exp emitter e1;
+      emitter (Push Rax);
+      codegen_exp emitter e2;
+      emitter (Push Rcx);
+      emitter (Cmpl (I 0, Ecx));
+      emitter (Setne Cl);
+      emitter (Cmpl (I 0, Eax));
+      emitter (Movl (0, Eax));
+      emitter (Setne Al);
+      emitter (Andb (Cl, Al))
+  | Logical_Or (e1, e2) ->
+      codegen_exp emitter e1;
+      emitter (Push Rax);
+      codegen_exp emitter e2;
+      emitter (Push Rcx);
+      emitter (Orl (Ecx, Eax));
+      emitter (Movl (0, Eax));
+      emitter (Setne Al)
+
 
 let codegen_stmt emitter = function
 | Return e -> 
