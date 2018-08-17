269e0bd55720c6de4e25efd31dadb50866904870
steve <stevejb71@gmail.com>
2018-08-17 22:59:55 +0800

    Update parser documentation

--- src/parser.mli
+++ src/parser.mli
@@ -2,15 +2,15 @@
 Grammar accepted by this parser (stage 4):
 
 <program> ::= <function>
-<function> ::= "int" <id> "(" ")" "{" <statement> "}"
-<statement> ::= "return" <exp> ";"
+<function> ::= "int" <id> "(" ")" "{" { <statement> } "}"
+<statement> ::= "return" <exp> ";" | <exp> ";" | int <id> = <exp>;
 <exp> ::= <logical-and-exp> { "||" <logical-and-exp> }
 <logical-and-exp> ::= <equality-exp> { "&&" <equality-exp> }
 <equality-exp> ::= <relational-exp> { ("!=" | "==") <relational-exp> }
 <relational-exp> ::= <additive-exp> { ("<" | ">" | "<=" | ">=") <additive-exp> }
 <additive-exp> ::= <term> { ("+" | "-") <term> }
 <term> ::= <factor> { ("*" | "/") <factor> }
-<factor> ::= "(" <exp> ")" | <unary_op> <factor> | <int> 
+<factor> ::= "(" <exp> ")" | <unary_op> <factor> | <int> | <id>
 *)
 
 open Ast
